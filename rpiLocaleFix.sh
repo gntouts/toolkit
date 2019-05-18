f57f3ec70f58f47ccbd771cacb5dcea009a10c0e
George Ntoutsos <g.ntouts@outlook.com>
2019-05-18 23:03:42 +0300

    Create rpiLocaleFix.sh
    How to fix perl warning "setting locale failed" in Raspbian.
    
    The warnings this script fixed for me:
    perl: warning: Setting locale failed.
    perl: warning: Please check that your locale settings:
    LC_**="en_GB.UTF-8"
    are supported and installed on your system.
    
    perl: warning: Falling back to the standard locale ("C").
    locale: Cannot set LC_**** to default locale: No such file or directory

---
+++ rpiLocaleFix.sh
@@ -0,0 +1,14 @@
+export LANGUAGE= en_GB.UTF-8
+export LC_ALL=en_GB.UTF-8
+export LC_TIME=en_GB.UTF-8
+export LC_MONETARY=en_GB.UTF-8
+export LC_ADDRESS=en_GB.UTF-8
+export LC_TELEPHONE=en_GB.UTF-8
+export LC_NAME=en_GB.UTF-8
+export LC_MEASUREMENT=en_GB.UTF-8
+export LC_IDENTIFICATION=en_GB.UTF-8
+export LC_NUMERIC=en_GB.UTF-8
+export LC_PAPER=en_GB.UTF-8
+export LANG=en_GB.UTF-8
+sudo locale-gen en_GB.UTF-8
+sudo dpkg-reconfigure locales
