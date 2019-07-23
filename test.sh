5c0283853066ac8fe31010a1eff98993aec0d9ad
George Ntoutsos <g.ntouts@outlook.com>
2019-07-23 17:34:57 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,6 @@
-echo Hi
+sudo echo Confirmed
+wget https://wire-app.wire.com/linux/debian/pool/main/Wire-3.9.2895_amd64.deb
+sudo dpkg -i Wire-3.9.2895_amd64.deb
+sudo apt install -f
+wire-desktop
+exit
