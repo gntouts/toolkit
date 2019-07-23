f149b29a8e8a40b0b7ac4963d9895bd315f07d11
George Ntoutsos <g.ntouts@outlook.com>
2019-07-23 17:44:34 +0300

    Update and rename Wire.sh to wire.sh

---
+++ wire.sh
@@ -0,0 +1,20 @@
+sudo echo Confirmed
+wget https://wire-app.wire.com/linux/debian/pool/main/Wire-3.9.2895_amd64.deb
+sudo dpkg -i Wire-3.9.2895_amd64.deb
+sudo apt install -f
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo GEIA SOU THODORAAAA
+"/opt/Wire/wire-desktop" %U
