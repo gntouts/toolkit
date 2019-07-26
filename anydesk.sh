5f41e184a41545b10efb07f045144daf2a6cf407
George Ntoutsos <g.ntouts@outlook.com>
2019-07-26 23:05:18 +0300

    Create anydesk.sh

---
+++ anydesk.sh
@@ -0,0 +1,7 @@
+sudo echo Hello Theodora!
+wget https://download.anydesk.com/linux/anydesk_5.1.1-1_amd64.deb
+sudo dpkg -i anydesk_5.1.1-1_amd64.deb
+sudo apt install -f
+sleep 1
+echo Done!
+sudo rm anydesk_5.1.1-1_amd64.deb
