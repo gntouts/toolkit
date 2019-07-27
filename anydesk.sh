d1816b791b23a5d80738dc9db4812e89d2d2b911
George Ntoutsos <g.ntouts@outlook.com>
2019-07-27 16:47:25 +0300

    Update anydesk.sh

--- anydesk.sh
+++ anydesk.sh
@@ -3,5 +3,5 @@ wget https://download.anydesk.com/linux/anydesk_5.1.1-1_amd64.deb
 sudo dpkg -i anydesk_5.1.1-1_amd64.deb
 sudo apt install -f
 sleep 1
-echo Done!
+echo Done
 sudo rm anydesk_5.1.1-1_amd64.deb
